0e536973503fd09ada379d6b201df2fd44d3fa17
LolitaBozhko <63454136+LolitaBozhko@users.noreply.github.com>
2020-04-13 13:38:42 +0300

    Add files via upload

---
+++ koren.cpp
@@ -0,0 +1,26 @@
+﻿// koren.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS"); 
+    float x; 
+    cout << "Введите значение x: ";
+    cin >> x;
+
+    float y = x, y1 = 0; 
+
+    while (true)
+    {
+        y1 = 0.5 * (y + 3 * x / (2 * y * y + x / y)); // итерационная формула
+
+        if (abs(y1 - y) < 10e-5) // условие окончание итерации
+            break;
+        y = y1; 
+    }
+    cout << "Корень кубический равен " << y1 << endl;
+}
